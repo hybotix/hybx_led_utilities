57de0d60608a9337cfe243a30a0892285a85ebbd
Dale Weber <dalew@hybridrobotix.org>
2026-04-11 14:18:28 -0700

    Added black lines to make code more readable.

--- src/hybx_led_utilities.c
+++ src/hybx_led_utilities.c
@@ -17,9 +17,11 @@
 
 void hybx_blink_led(uint8_t pin, uint16_t delay_ms, uint8_t nr_cycles) {
     uint16_t cycles = 0;
+
     for (cycles = 0; cycles < nr_cycles; cycles++) {
         gpio_set_level(pin, HIGH);
         vTaskDelay(delay_ms / portTICK_PERIOD_MS);
+
         gpio_set_level(pin, LOW);
         vTaskDelay(delay_ms / portTICK_PERIOD_MS);
     }
@@ -35,6 +37,7 @@ void hybx_init_rgb_led(void) {
         .freq_hz         = 5000,
         .clk_cfg         = LEDC_AUTO_CLK,
     };
+
     ledc_timer_config(&rgb_timer);
 
     ledc_channel_config_t rgb_red_ch = {
@@ -45,6 +48,7 @@ void hybx_init_rgb_led(void) {
         .duty       = 255,
         .hpoint     = 0,
     };
+
     ledc_channel_config_t rgb_green_ch = {
         .gpio_num   = LED_G,
         .speed_mode = LEDC_LOW_SPEED_MODE,
@@ -53,6 +57,7 @@ void hybx_init_rgb_led(void) {
         .duty       = 255,
         .hpoint     = 0,
     };
+
     ledc_channel_config_t rgb_blue_ch = {
         .gpio_num   = LED_B,
         .speed_mode = LEDC_LOW_SPEED_MODE,
@@ -61,6 +66,7 @@ void hybx_init_rgb_led(void) {
         .duty       = 255,
         .hpoint     = 0,
     };
+
     ledc_channel_config(&rgb_red_ch);
     ledc_channel_config(&rgb_green_ch);
     ledc_channel_config(&rgb_blue_ch);
@@ -74,10 +80,13 @@ void hybx_set_rgb_color(uint32_t color) {
     uint8_t red   = (color >> 16) & 0xFF;
     uint8_t green = (color >> 8)  & 0xFF;
     uint8_t blue  =  color        & 0xFF;
+
     ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 255 - red);
     ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+
     ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 255 - green);
     ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1);
+
     ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_2, 255 - blue);
     ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_2);
 }
@@ -94,10 +103,13 @@ void hybx_turn_rgb_off(void) {
 
 void hybx_blink_led_rgb(uint32_t color, uint16_t delay_ms, uint8_t nr_cycles) {
     uint8_t cycles = 0;
+
     hybx_set_rgb_color(color);
+
     for (cycles = 0; cycles < nr_cycles; cycles++) {
         hybx_turn_rgb_on(color);
         vTaskDelay(delay_ms / portTICK_PERIOD_MS);
+
         hybx_turn_rgb_off();
         vTaskDelay(delay_ms / portTICK_PERIOD_MS);
     }
